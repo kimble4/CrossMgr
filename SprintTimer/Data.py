806c4b6f5a65a45a1e45a4e5e0044cf50097eebd
kimble4 <kim@ductilebiscuit.net>
2024-01-14 18:25:35 +0000

    Flags in SprintTimer UI

--- SprintTimer/Data.py
+++ SprintTimer/Data.py
@@ -14,6 +14,63 @@ from HighPrecisionTimeEdit import HighPrecisionTimeEdit
 
 import Utils
 import Model
+import Flags
+
+bitmapCache = {}
+class IOCCodeRenderer(wx.grid.GridCellRenderer):
+	def getImgWidth( self, ioc, height ):
+		img = Flags.GetFlagImage( ioc )
+		if img:
+			imgHeight = int( height * 0.8 )
+			imgWidth = int( float(img.GetWidth()) / float(img.GetHeight()) * float(imgHeight) )
+			padding = int(height * 0.1)
+			return img, imgWidth, imgHeight, padding
+		return None, 0, 0, 0
+
+	def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+		text = grid.GetCellValue(row, col)
+
+		dc.SetFont( attr.GetFont() )
+		w, h = dc.GetTextExtent( text )
+		
+		ioc = text[:3]
+		img, imgWidth, imgHeight, padding = self.getImgWidth(ioc, h)
+		
+		fg = attr.GetTextColour()
+		bg = attr.GetBackgroundColour()
+		if isSelected:
+			fg, bg = bg, fg
+		
+		dc.SetBrush( wx.Brush(bg, wx.SOLID) )
+		dc.SetPen( wx.TRANSPARENT_PEN )
+		dc.DrawRectangle( rect )
+
+		rectText = wx.Rect( rect.GetX()+padding+imgWidth, rect.GetY(), rect.GetWidth()-padding-imgWidth, rect.GetHeight() )
+		
+		hAlign, vAlign = attr.GetAlignment()
+		dc.SetTextForeground( fg )
+		dc.SetTextBackground( bg )
+		grid.DrawTextRectangle(dc, text, rectText, hAlign, vAlign)
+
+		if img:
+			key = (ioc, imgHeight)
+			if key not in bitmapCache:
+				bitmapCache[key] = img.Scale(imgWidth, imgHeight, wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
+			dc.DrawBitmap( bitmapCache[key], rect.GetX(), rect.GetY()+(rect.GetHeight()-imgHeight)//2 )
+
+	def GetBestSize(self, grid, attr, dc, row, col):
+		text = grid.GetCellValue(row, col)
+		dc.SetFont(attr.GetFont())
+		w, h = dc.GetTextExtent( text )
+		
+		img, imgWidth, imgHeight, padding = self.getImgWidth(text[:3], h)
+		if img:
+			return wx.Size(w + imgWidth + padding, h)
+		else:
+			return wx.Size(w, h)
+
+	def Clone(self):
+		return IOCCodeRenderer()
 
 class BibEntry( wx.Panel ):
 	def __init__( self, parent, id = wx.ID_ANY ):
@@ -727,6 +784,7 @@ class Data( wx.Panel ):
 			elif "sprintNatcodeEdited" in sprintDict:
 				natcode = sprintDict["sprintNatcodeEdited"]
 				self.dataGrid.SetCellBackgroundColour(row, col, self.orangeColour)
+			self.dataGrid.SetCellRenderer(row, col, IOCCodeRenderer() )
 			self.dataGrid.SetCellValue(row, col, natcode)
 			self.dataGrid.SetCellAlignment(row, col, wx.ALIGN_LEFT, wx.ALIGN_CENTER)
 			col += 1
--- SprintTimer/Results.py
+++ SprintTimer/Results.py
@@ -13,63 +13,63 @@ from FixCategories import FixCategories, SetCategory
 #from EditEntry import CorrectNumber, ShiftNumber, InsertNumber, DeleteEntry, SwapEntry
 from Undo import undo
 import datetime
-#import Flags
+import Flags
 
-#bitmapCache = {}
-#class IOCCodeRenderer(wx.grid.GridCellRenderer):
-	#def getImgWidth( self, ioc, height ):
-		#img = Flags.GetFlagImage( ioc )
-		#if img:
-			#imgHeight = int( height * 0.8 )
-			#imgWidth = int( float(img.GetWidth()) / float(img.GetHeight()) * float(imgHeight) )
-			#padding = int(height * 0.1)
-			#return img, imgWidth, imgHeight, padding
-		#return None, 0, 0, 0
+bitmapCache = {}
+class IOCCodeRenderer(wx.grid.GridCellRenderer):
+	def getImgWidth( self, ioc, height ):
+		img = Flags.GetFlagImage( ioc )
+		if img:
+			imgHeight = int( height * 0.8 )
+			imgWidth = int( float(img.GetWidth()) / float(img.GetHeight()) * float(imgHeight) )
+			padding = int(height * 0.1)
+			return img, imgWidth, imgHeight, padding
+		return None, 0, 0, 0
 
-	#def Draw(self, grid, attr, dc, rect, row, col, isSelected):
-		#text = grid.GetCellValue(row, col)
+	def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+		text = grid.GetCellValue(row, col)
 
-		#dc.SetFont( attr.GetFont() )
-		#w, h = dc.GetTextExtent( text )
+		dc.SetFont( attr.GetFont() )
+		w, h = dc.GetTextExtent( text )
 		
-		#ioc = text[:3]
-		#img, imgWidth, imgHeight, padding = self.getImgWidth(ioc, h)
+		ioc = text[:3]
+		img, imgWidth, imgHeight, padding = self.getImgWidth(ioc, h)
 		
-		#fg = attr.GetTextColour()
-		#bg = attr.GetBackgroundColour()
-		#if isSelected:
-			#fg, bg = bg, fg
+		fg = attr.GetTextColour()
+		bg = attr.GetBackgroundColour()
+		if isSelected:
+			fg, bg = bg, fg
 		
-		#dc.SetBrush( wx.Brush(bg, wx.SOLID) )
-		#dc.SetPen( wx.TRANSPARENT_PEN )
-		#dc.DrawRectangle( rect )
+		dc.SetBrush( wx.Brush(bg, wx.SOLID) )
+		dc.SetPen( wx.TRANSPARENT_PEN )
+		dc.DrawRectangle( rect )
 
-		#rectText = wx.Rect( rect.GetX()+padding+imgWidth, rect.GetY(), rect.GetWidth()-padding-imgWidth, rect.GetHeight() )
+		rectText = wx.Rect( rect.GetX()+padding+imgWidth, rect.GetY(), rect.GetWidth()-padding-imgWidth, rect.GetHeight() )
 		
-		#hAlign, vAlign = attr.GetAlignment()
-		#dc.SetTextForeground( fg )
-		#dc.SetTextBackground( bg )
-		#grid.DrawTextRectangle(dc, text, rectText, hAlign, vAlign)
+		hAlign, vAlign = attr.GetAlignment()
+		dc.SetTextForeground( fg )
+		dc.SetTextBackground( bg )
+		grid.DrawTextRectangle(dc, text, rectText, hAlign, vAlign)
 
-		#if img:
-			#key = (ioc, imgHeight)
-			#if key not in bitmapCache:
-				#bitmapCache[key] = img.Scale(imgWidth, imgHeight, wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
-			#dc.DrawBitmap( bitmapCache[key], rect.GetX(), rect.GetY()+(rect.GetHeight()-imgHeight)//2 )
+		if img:
+			key = (ioc, imgHeight)
+			if key not in bitmapCache:
+				bitmapCache[key] = img.Scale(imgWidth, imgHeight, wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
+			dc.DrawBitmap( bitmapCache[key], rect.GetX(), rect.GetY()+(rect.GetHeight()-imgHeight)//2 )
 
-	#def GetBestSize(self, grid, attr, dc, row, col):
-		#text = grid.GetCellValue(row, col)
-		#dc.SetFont(attr.GetFont())
-		#w, h = dc.GetTextExtent( text )
+	def GetBestSize(self, grid, attr, dc, row, col):
+		text = grid.GetCellValue(row, col)
+		dc.SetFont(attr.GetFont())
+		w, h = dc.GetTextExtent( text )
 		
-		#img, imgWidth, imgHeight, padding = self.getImgWidth(text[:3], h)
-		#if img:
-			#return wx.Size(w + imgWidth + padding, h)
-		#else:
-			#return wx.Size(w, h)
+		img, imgWidth, imgHeight, padding = self.getImgWidth(text[:3], h)
+		if img:
+			return wx.Size(w + imgWidth + padding, h)
+		else:
+			return wx.Size(w, h)
 
-	#def Clone(self):
-		#return IOCCodeRenderer()
+	def Clone(self):
+		return IOCCodeRenderer()
 
 #reNonDigits = re.compile( '[^0-9]' )
 #reLapMatch = re.compile( '<?Lap>? ([0-9]+)' )
@@ -844,6 +844,7 @@ class Results( wx.Panel ):
 				col += 1
 				self.resultsGrid.SetCellValue(row, col, str(gender))
 				col += 1
+				self.resultsGrid.SetCellRenderer(row, col, IOCCodeRenderer() )
 				self.resultsGrid.SetCellValue(row, col, str(natcode))
 				col += 1				
 				self.resultsGrid.SetCellValue(row, col, str('{:.3f}'.format(sprintDict["sprintTime"])))
@@ -889,6 +890,7 @@ class Results( wx.Panel ):
 				col += 1
 				self.resultsGrid.SetCellValue(row, col, str(gender))
 				col += 1
+				self.resultsGrid.SetCellRenderer(row, col, IOCCodeRenderer() )
 				self.resultsGrid.SetCellValue(row, col, str(natcode))
 				col += 5
 				self.resultsGrid.SetCellValue(row, col, '0')
